52248af8b2a02779e74462ab04f073635969a639
ebbakronw <ebba.kronwall@medieinstitutet.se>
2022-02-26 12:21:40 +0100

    börjat på footern

--- template-react-webshop/src/components/Footer.js
+++ template-react-webshop/src/components/Footer.js
@@ -1,8 +1,18 @@
 import React from 'react'
 
+
 function Footer() {
   return (
-    <div>Footer</div>
+    <div className='footer-container' style={{height:"100px"}}>
+      
+      <h1 style={{"textAlign": "center", "fontSize": "18px"}}>Cake House Stockholm</h1>
+      <p style={{"textAlign": "center", "fontSize": "12px"}}>
+      Upplandsgatan 38, 113 12 Stockholm
+      <br/>
+      070-56 67 45
+      </p>
+      <p style={{"textAlign": "center", "fontSize": "8px"}}>Copyright &copy; </p>
+    </div>
   )
 }
 
