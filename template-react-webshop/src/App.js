57748b86b9595a18b5e7b4e058cd236a73843289
ebbakronw <ebba.kronwall@medieinstitutet.se>
2022-02-28 16:14:11 +0100

    starting to code product page

--- template-react-webshop/src/App.js
+++ template-react-webshop/src/App.js
@@ -1,12 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Footer from './components/Footer'
+import Product from './pages/Product';
+import Products from './pages/Products';
+import Checkout from './pages/Checkout'
 
 function App() {
   return (
     <div className="App">
       Webshop in React
+    <BrowserRouter>
+
+    <Routes>
+      <Route path="/" element={<Products />}/>
+      <Route path="/products/:id" element={<Product />} />
+      <Route path="/checkout" element={<Checkout />}/>
+    </Routes>
+    
+    <Footer />
+    
+    </BrowserRouter>
     </div>
   );
 }
--- template-react-webshop/src/pages/Product.js
+++ template-react-webshop/src/pages/Product.js
@@ -1,9 +1,45 @@
-import React from 'react'
+import React, {useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
 
 function Product() {
+  const [cake, setCake] = useState({});
+  const params = useParams();
+  
+  const fetchData = async () => {
+    try {
+    const response = await fetch('https://codexplained.se/cakes.php?id=234967');
+    const data = await response.json();
+    console.log(data);
+    
+    setCake(data);
+    
+    } catch(error) {
+     console.log(error);
+    }
+   
+  }
+    useEffect(() => {
+     fetchData();
+    }, []);
+
   return (
-    <div>Product</div>
+    <div>
+      
+        <article key={cake.id}>
+          <img src={cake.url} alt="picture of cake" /> 
+          <p>{cake.title}</p>
+          <p>{cake.price}</p>
+          <p>{cake.description}</p>
+          <p>{cake.storage}</p>
+          <button>Add to Cart</button>
+        </article>
+
+    <h1>cake with id: {params.id}</h1>
+    
+    </div>
   )
-}
+  }
+
 
 export default Product
