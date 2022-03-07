afbadbd2df5be85ad7da8b0d46af023b510b4916
ebbakronw <ebba.kronwall@medieinstitutet.se>
2022-03-07 10:11:52 +0100

    style the checkout-page

--- template-react-webshop/src/pages/Checkout.js
+++ template-react-webshop/src/pages/Checkout.js
@@ -13,76 +13,79 @@ function Checkout({items, sum}) {
   <div className='products-checkout'>
 
        {items.length < 1 ? 
-       <div>
-            <h4>Det finns inga produkter</h4>
-       <Link to="/"><button>Back</button></Link>
+       <div className='no-products'>
+            <h4 className='no-products-text'>No products</h4>
+            <Link to="/"><button className='home-checkout-btn'>Back</button></Link>
        </div> 
        :
        <div className='products-description'>
- {
+           {
       items.map(item => (
            <table>
                 <tr>
                 <td><img src={item.url} alt='image' className='checkout-img'></img></td>
-                <td><h1>{item.title}</h1></td>
-                <td><h3>{item.price}</h3></td>
+                <td><h1 className='checkout-title'>{item.title}</h1></td>
+                <td><h3 className='checkout-price'>{item.price}</h3></td>
                 </tr>
            </table>
       ))}
-      <div className='total'>Total price: {sum}</div>
+      <div className='total-price'>Total price: {sum}</div>
+
+
+      <div className='input-container'>
+
+<div className='checkout-container'>
+     <label> Firstname</label>
+     <input type="text" namn="firstname" className='form-control' />
+</div>
+
+<div className='checkout-container'>
+     <label> Lastname</label>
+     <input type="text" namn="lastname" className='form-control' />
+</div>
+
+<div className='checkout-container'>
+     <label> Phone-number </label>
+     <input type="text" namn="phone" className='form-control' />
+</div>
+
+<div className='checkout-container'>
+     <label> City </label>
+     <input type="text" namn="city" className='form-control' />
+</div>
+
+<div className='checkout-container'>
+     <label> State</label>
+     <input type="text" namn="state" className='form-control' />
+</div>
+
+<div className='checkout-container'>
+     <label> Zip Code</label>
+     <input type="text" namn="lastzipcode" className='form-control' />
+</div>
+
+</div>
+
+
+<div className='checkout-payments'>
+        <h3>Payment</h3>
+        <label for="fname">Accepted Cards</label>
+        <div class="icon-container">
+        <img src="https://img.icons8.com/ios/50/000000/visa.png"/>
+        <img src="https://img.icons8.com/fluency/48/000000/mastercard.png"/>
+        <img src="https://img.icons8.com/ios/50/000000/amex.png"/>
+</div>
+</div>
+
+<div className='checkout-container'>
+     <button type='button' className='checkout-btn'> Place Order</button>
+</div>
  </div>
 }
  </div>
     
 
-    <div className='input-container'>
-
-    <div className='checkout-container'>
-         <label> Firstname</label>
-         <input type="text" namn="firstname" className='form-control' />
-    </div>
-
-    <div className='checkout-container'>
-         <label> Lastname</label>
-         <input type="text" namn="lastname" className='form-control' />
-    </div>
-
-    <div className='checkout-container'>
-         <label> Phone-number </label>
-         <input type="text" namn="phone" className='form-control' />
-    </div>
-
-    <div className='checkout-container'>
-         <label> City </label>
-         <input type="text" namn="city" className='form-control' />
-    </div>
-
-    <div className='checkout-container'>
-         <label> State</label>
-         <input type="text" namn="state" className='form-control' />
-    </div>
-
-    <div className='checkout-container'>
-         <label> Zip Code</label>
-         <input type="text" namn="lastzipcode" className='form-control' />
-    </div>
-
-    </div>
-
-
-    <div className='checkout-payments'>
-            <h3>Payment</h3>
-            <label for="fname">Accepted Cards</label>
-            <div class="icon-container">
-            <img src="https://img.icons8.com/ios/50/000000/visa.png"/>
-            <img src="https://img.icons8.com/fluency/48/000000/mastercard.png"/>
-            <img src="https://img.icons8.com/ios/50/000000/amex.png"/>
-    </div>
-    </div>
    
-    <div className='checkout-container'>
-         <button type='button' className='checkout-btn'> Place Order</button>
-    </div>
    
     </div>
   
